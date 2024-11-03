109ac3b80a6b8b8fd1302885a2cf7330d39b7a24
JP <oyomthojohnpaul@gmail.com>
2024-11-03 22:07:28 +0300

    Fix borderRadius

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -64,8 +64,8 @@ module.exports = {
   			}
   		},
   		borderRadius: {
-  			lg: '`var(--radius)`',
-  			md: '`calc(var(--radius) - 2px)`',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		fontFamily: {
