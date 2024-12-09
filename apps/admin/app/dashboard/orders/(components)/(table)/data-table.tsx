b80d9c3c73ce816f6bdf4bd4382415d8f8e40ecb
JP <oyomthojohnpaul@gmail.com>
2024-12-09 11:48:44 +0300

    Update copy

--- apps/admin/app/dashboard/orders/(components)/(table)/data-table.tsx
+++ apps/admin/app/dashboard/orders/(components)/(table)/data-table.tsx
@@ -155,7 +155,7 @@ export function DataTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No products.
+                  No orders.
                 </TableCell>
               </TableRow>
             )}
