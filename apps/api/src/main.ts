2c868812ab442f4b274bf602011e7cb45081c06f
JP <oyomthojohnpaul@gmail.com>
2024-12-11 23:44:31 +0300

    update app initialization

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,29 +3,26 @@ import { AppModule } from '@/api/app/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
-let app: any;
-
 async function bootstrap() {
-  app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'graphql';
   app.use(cookieParser(process.env.COOKIE_SECRET!));
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
-  
+
   const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
   app.enableCors({
     origin: allowedOrigins,
-    credentials: true
+    credentials: true,
   });
 
   const port = process.env.PORT! || 4100;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-    'NestApplication',
+    'NestApplication'
   );
   return app;
 }
-bootstrap();
 
-module.exports = app;
+bootstrap();
